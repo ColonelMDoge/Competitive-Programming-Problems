3bdf87e1470a2e72054a567d5359db7a837a2191
Alex Zhou <160194729+ColonelMDoge@users.noreply.github.com>
2025-02-16 15:19:03 -0500

    Added CCC'13'S1 From 1987 To 2013

---
+++ src/ccc/CCC13S1From1987To2013.java
@@ -0,0 +1,22 @@
+package ccc._2013;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class CCC13S1From1987To2013 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int start = input.nextInt() + 1;
+        HashSet<Character> dupes = new HashSet<>();
+        while(true) {
+            for (char c : String.valueOf(start).toCharArray()) dupes.add(c);
+            if (dupes.size() == String.valueOf(start).length()) {
+                 System.out.println(start);
+                 break;
+            } else {
+                start++;
+                dupes.clear();
+            }
+        }
+    }
+}
