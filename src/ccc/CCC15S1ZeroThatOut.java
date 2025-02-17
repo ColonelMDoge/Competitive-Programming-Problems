a98539d566be04925e2396f1ebfd4ce196d04ac7
Alex Zhou <160194729+ColonelMDoge@users.noreply.github.com>
2025-02-16 21:39:25 -0500

    Added CCC'15'S1 Zero That Out

---
+++ src/ccc/CCC15S1ZeroThatOut.java
@@ -0,0 +1,20 @@
+package ccc._2015;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class CCC15S1ZeroThatOut {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<>();
+        int number = input.nextInt();
+        for (int i = 0; i < number; i++) {
+            stack.push(input.nextInt());
+            if (stack.peek() == 0) {
+                stack.pop();
+                stack.pop();
+            }
+        }
+        System.out.println(stack.stream().mapToInt(Integer::intValue).sum());
+    }
+}
