3437c01e03893fed17fff825f120f6cb26fc7dbe
Alex Zhou <160194729+ColonelMDoge@users.noreply.github.com>
2025-02-16 20:14:15 -0500

    Added CCC'11'S1 English Or French

---
+++ src/CCC11S1EnglishOrFrench.java
@@ -0,0 +1,17 @@
+package ccc._2011;
+
+import java.util.Scanner;
+
+public class CCC11S1EnglishOrFrench {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int lines = input.nextInt(), t = 0, s = 0;
+        for (int i = 0; i < lines; i++) {
+            String sentence = input.nextLine();
+            t += (int) sentence.chars().filter(e -> e == 't'|| e == 'T').count();
+            s += (int) sentence.chars().filter(e -> e == 's' || e == 'S').count();
+        }
+        if (t >= s) System.out.println("English");
+        else System.out.println("French");
+    }
+}
